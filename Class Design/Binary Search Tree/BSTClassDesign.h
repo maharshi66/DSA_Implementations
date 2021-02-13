363a2b24a1a55ccf2f563c26059e1d70e0d1e0e3
Maharshi Shah <40323681+maharshi66@y.github.com>
2021-02-13 18:09:33 +0530

    Implemented Search and Delete by key value

--- Class Design/Binary Search Tree/BSTClassDesign.h
+++ Class Design/Binary Search Tree/BSTClassDesign.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
+using namespace std;
 //BST Node
 struct TreeNode{
 	int val;
@@ -14,32 +14,24 @@ struct TreeNode{
 
 class BinarySearchTree{
 	private:    
-    void helper(TreeNode* root, vector<int>& result)
+    void preOrderTraversal_wrapper(TreeNode* root, vector<int>& result)
     {
         if(root != nullptr)
         {
             if(root->left != nullptr)
             {
-                helper(root->left, result);
+                preOrderTraversal_wrapper(root->left, result);
             }
             
             result.push_back(root->val);
             
             if(root->right != nullptr)
             {
-                helper(root->right, result);
+                preOrderTraversal_wrapper(root->right, result);
             }
         }
     }
 
-	public:
-    TreeNode* root;
-    BinarySearchTree(int val)
-    {
-        root = new TreeNode(val);
-    }
-
-    //BST Insert, Search, Delete
     TreeNode* insertNode(TreeNode* node, int val){
         
         if(node == nullptr){
@@ -56,18 +48,94 @@ class BinarySearchTree{
         return node;
     }
 
-    TreeNode* insert(int key){
-        root = insertNode(this->root, key);
-        return root;
+    bool searchNode(TreeNode* node, int val){
+        if(node == nullptr) return false;
+
+        if(node->val == val) return true;
+        else if(node->val > val){
+            return searchNode(node->left, val);
+        }else if(node->val < val){
+            return searchNode(node->right, val);
+        }
+        return false;
+    }
+
+    TreeNode* deleteNode(TreeNode* node, int key){
+        if(node == nullptr){
+            return nullptr;
+        }
+
+        if(key < node->val){
+            node->left=deleteNode(node->left,key);
+            return node;
+        }
+        if(key > node->val){
+            node->right = deleteNode(node->right,key);
+            return node;
+        }
+        if(node->val == key){
+            //case 1.node has no children
+            if(node->left == nullptr and node->right == nullptr){
+                delete node;
+                return nullptr;
+            }   
+            
+            //case 2. node has one child
+            if(node->left != nullptr and node->right == nullptr){
+                TreeNode*temp = node->left;
+                delete node;
+                return temp;
+            }
+            
+            if(node->right != nullptr and node->left == nullptr){
+                TreeNode*temp=node->right;
+                delete node;
+                return temp;
+            } 
+            
+            //case 3. node has 2 children
+            TreeNode*replace = node->right;
+            while( replace->left != nullptr){
+                replace = replace->left;
+            }
+            
+            node->val = replace->val;
+            node->right = deleteNode(node->right,replace->val);
+            return node;
+        }
+        return nullptr;
+    }
+
+	public:
+    TreeNode* root;
+    BinarySearchTree() {}   
+    BinarySearchTree(int val)
+    {
+        root = new TreeNode(val);
+    }
+
+    //BST Insert, Search, Delete by key
+    TreeNode* insertKey(int key){
+        return insertNode(this->root, key);
+    }
+
+    bool searchKey(int key){
+        return searchNode(this->root, key);
     } 	
+		
+	void deleteKey(int key){
+        if(searchKey(key)){
+            deleteNode(this->root, key);        
+        }else{
+            cout<<"Key not present in the tree!"<<endl;
+        }
+        return;
+    }
     
-    BinarySearchTree() {} 	
-	
-	void deleteNode(int val);
-	bool searchNode(int val);
+    //Traversal methods
     vector<int> preOrderTraversal() {
         vector<int> resultVector;
-        helper(this->root, resultVector);
+        preOrderTraversal_wrapper(this->root, resultVector);
         return resultVector;               
     }
 };
--- Class Design/Binary Search Tree/BSTClassImplement.cpp
+++ Class Design/Binary Search Tree/BSTClassImplement.cpp
@@ -3,14 +3,29 @@ using namespace std;
 
 int main(){
     BinarySearchTree *Tree = new BinarySearchTree(5);
-    Tree->root->left = new TreeNode(2);
-    Tree->root->right = new TreeNode(6);
-    Tree->insert(8);
+    Tree->insertKey(8);
+    Tree->insertKey(2);
+    Tree->insertKey(6);
+    Tree->insertKey(44);
+    Tree->insertKey(-1);
     vector<int> nodes(Tree->preOrderTraversal());
-    
-    for(auto i : nodes){
+  	
+ //Print Preorder (Ascending in BST)
+/*    for(auto i : nodes){
         cout << i <<", ";
     }
     cout<<endl;
+
+    cout<<"Search result: "<<Tree->searchKey(-1)<<endl;
+    cout<<"Search result: "<<Tree->searchKey(88)<<endl;
+    
+    Tree->deleteKey(-1);
+    vector<int> newnodes(Tree->preOrderTraversal());
+
+    for(auto i : newnodes){
+        cout << i <<", ";
+    }
+    cout<<endl;*/
+    
     return 0;
 }
